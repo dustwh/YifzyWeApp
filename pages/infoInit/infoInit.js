fd9d303c6d578885d3e031d6e332308d6095c239
dustwh <32893851+dustwh@users.noreply.github.com>
2020-02-09 00:56:44 +0800

    Update Infoint
    Done:Now can update info if info not complete.Form function all done.
    Remain:Pull student info before fill in.

--- pages/infoInit/infoInit.js
+++ pages/infoInit/infoInit.js
@@ -1,24 +1,33 @@
 // pages/infoInit/infoInit.js
+const app = getApp()
 Page({
   data: {
-    showSub:false,
+    showSub: false,
     switch1: false,
-    tel:"",
-    mark:"",
-    school:"",
+    tel: "",
+    mark: "",
+    school: "",
     region: ['', '', ''],
-    regionCode:[],
-    grade: ['点击选择','高 1', '高 2', '高 3'],
-    subArray: ['选择文理','文科','理科'],
+    regionCode: "",
+    grade: ['点击选择', '高 1', '高 2', '高 3'],
+    subArray: ['选择文理', '文科', '理科'],
+    schoolArray: [
+      {
+        id: 0,
+        name: '点击选择学校'
+      }
+    ],
     gradeIndex: 0,
-    subIndex: 0
+    subIndex: 0,
+    schoolIndex: 0,
+    isSetPlace: false
   },
   onChange(event) {
     const detail = event.detail;
     this.setData({
       'switch1': detail.value
     })
-    if(detail.value==3){
+    if (detail.value == 3) {
       this.setData({
         'showSub': true
       })
@@ -30,86 +39,119 @@ Page({
    * 生命周期函数--监听页面加载
    */
   bindPickerChange: function (e) {
-    console.log('picker1发送选择改变，携带值为', e.detail.value)
-    
+    // console.log('picker1发送选择改变，携带值为', e.detail.value)
+
     this.setData({
       gradeIndex: e.detail.value
     })
-    if(e.detail.value==3){
+    if (e.detail.value == 3) {
       this.setData({
         showSub: true
       })
-    }else{
+    } else {
       this.setData({
         showSub: false
       })
     }
   },
   bindSubChange: function (e) {
-    console.log('picker sub发送选择改变，携带值为', e.detail.value)
+    // console.log('picker sub发送选择改变，携带值为', e.detail.value)
     this.setData({
       subIndex: e.detail.value
     })
   },
   bindRegionChange: function (e) {
+    var districCode = e.detail.code[2]
+    var schoolCodesArray = app.globalData.highschoolIndex[districCode]
+    var schoolNamesArray = [
+      {
+        id: 0,
+        name: '点击选择学校'
+      }
+    ]
+    
+    for (var i in schoolCodesArray) {
+      var shcoolTmp = {
+        id: schoolCodesArray[i],
+        name: app.globalData.highschoolDictionary[schoolCodesArray[i]]
+      }
+      schoolNamesArray.push(shcoolTmp)
+    }
+    // console.log(schoolNamesArray)
     this.setData({
       region: e.detail.value,
-      regionCode: e.detail.code
+      regionCode: e.detail.code,
+      isSetPlace: true,
+      schoolArray: schoolNamesArray
     })
-    console.log("更改为:" + e.detail.code)
   },
-  confirm:function(){
+  bindSchoolChange:function (e) {
+    // console.log(e.detail)
+    this.setData({
+      schoolIndex: e.detail.value
+    })
+  },
+  confirm: function () {
     var that = this
-    console.log(this.data.tel)
-    console.log(this.data.mark)
-    console.log(2020-parseInt(this.data.gradeIndex))
-    var subjectcode=0
-    subjectcode=this.data.subIndex
-    if(subjectcode==2){
-      subjectcode=5
+    var telInfo = this.data.tel
+    var markInfo = this.data.mark
+    var yearInfo = 2020 - parseInt(this.data.gradeIndex)
+    var subjectcode = 0
+    subjectcode = this.data.subIndex
+    if (subjectcode == 2) {
+      subjectcode = 5
     }
+    var provinceInfo = this.data.regionCode[0]
+    var cityInfo = this.data.regionCode[1]
+    var districtInfo = this.data.regionCode[2]
+    var schoolInfo = this.data.schoolArray[this.data.schoolIndex].id
+
+    console.log(telInfo)
+    console.log(markInfo)
+    console.log(yearInfo)
     console.log(subjectcode)
-    console.log(this.data.regionCode)
-    console.log(this.data.school)
+    console.log(provinceInfo)
+    console.log(cityInfo)
+    console.log(districtInfo)
+    console.log(schoolInfo)
+    // need check
+    wx.request({
+      url: "http://localhost:8080/weapp/wxSaveInitInfo",
+      method: "POST",
+      data: {
+        telInfo:telInfo,
+        markInfo:markInfo,
+        yearInfo:yearInfo,
+        subjectcode:subjectcode,
+        provinceInfo:provinceInfo,
+        cityInfo:cityInfo,
+        districtInfo:districtInfo,
+        schoolInfo:schoolInfo
+      },
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      success: function (res) {
+        if (res.statusCode == '200') {
+          console.log(res.data)
+          if (res.data == "success") {
+            wx.setStorageSync("phone", that.data.tel)
+            wx.redirectTo({
+              url: '../homepage/homepage',
+            })
+          } else {
+            console.log("服务器错误");
+          }
+        } else {
+          console.log("网络似乎出小差，请检查wifi或流量")
 
-    // console.log(this.data.switch1+"")
-
-    // wx.request({
-    //   url: "http://localhost:8080/wxSaveInitInfo",
-    //   method: "POST",
-    //   data: {
-    //     tel: that.data.tel,
-    //     name: that.data.mark,
-    //     tmpplace: that.data.school,
-    //     isnewce: that.data.switch1+""
-    //   },
-    //   header: {
-    //     "Content-Type": "application/x-www-form-urlencoded"
-    //   },
-    //   success: function (res) {
-    //     if (res.statusCode == '200') {
-    //       console.log(res.data)
-    //       if (res.data == "success") {
-    //         wx.setStorageSync("tel", that.data.tel)
-    //         wx.navigateTo({
-    //           url: '../homepage/homepage',
-    //         })
-    //       } else {
-    //         console.log("没返回");
-    //       }
-    //     } else {
-    //       console.log("通讯失败")
-    //       // $Message({
-    //       //   content: '与服务器通讯失败',
-    //       //   type: 'error'
-    //       // });
-    //     }
-    //   },
-    // })
-    
-    // wx.navigateTo({
-    //   url: '../homepage/homepage',
-    // })
+          // $Message({
+          //   content: '与服务器通讯失败',
+          //   type: 'error'
+          // });
+        }
+      },
+    })
   },
   onLoad: function () {
     var phone = wx.getStorageSync("phone")
@@ -118,7 +160,7 @@ Page({
     });
     console.log("拉取数据")
   },
-  
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -167,17 +209,17 @@ Page({
   onShareAppMessage: function () {
 
   },
-  bindKeyInput1:function(e) {
+  bindKeyInput1: function (e) {
     this.setData({
       tel: e.detail.value
     })
   },
-  bindKeyInput2:function(e) {
+  bindKeyInput2: function (e) {
     this.setData({
       mark: e.detail.value
     })
   },
-  bindKeyInput3:function(e) {
+  bindKeyInput3: function (e) {
     this.setData({
       school: e.detail.value
     })
