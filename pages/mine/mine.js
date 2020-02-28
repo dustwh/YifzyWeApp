10cc3e423f0016b1100272bf19e30d988a5dbabc
dustwh <32893851+dustwh@users.noreply.github.com>
2020-02-28 12:56:30 +0800

    done UI effect

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -6,6 +6,18 @@ Page({
    */
   data: {
     current: 'mine',
+    name:"",
+    phone:"",
+    newCELabel:"",
+    chineseMark:0,
+    mathMark:0,
+    flMark:0,
+    sub1:"",
+    sub2:"",
+    sub3:"",
+    sub1mark:0,
+    sub2mark:0,
+    sub3mark:0,
     total:750,
     rank:123456
   },
@@ -30,7 +42,44 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    var sessionId = wx.getStorageSync("sessionId")
+    wx.request({
+      url: 'http://localhost:8080/weapp/minePageInfoOnload',
+      header: {
+        'content-type': 'application/json',
+        'Cookie': sessionId
+      },
+      success: function (res) {
+        console.log(res.data)
+        var name=res.data.name
+        var newCELabel=res.data.newCELabel
+        var sub1=res.data.sub1
+        var sub2=res.data.sub2
+        var sub3=res.data.sub3
+        var sub1mark=res.data.sub1mark
+        var sub2mark=res.data.sub2mark
+        var sub3mark=res.data.sub3mark
+        var phone = wx.getStorageSync('phone');
+        that.setData({
+          name:name,
+          phone: phone,
+          newCELabel:newCELabel,
+          sub1:sub1,
+          sub2:sub2,
+          sub3:sub3,
+          chineseMark:222,
+          mathMark:222,
+          flMark:222,
+          sub1mark:111,
+          sub2mark:222,
+          sub3mark:333
+        });
+      },
+      fail: function () {
+        console.log("fail")
+      }
+    })
   },
 
   show_wc:function(){
